cc3d833b55853b352be88a5ea4ed45b8dbad2b03
art2611 <art261197@hotmail.fr>
2022-02-07 17:07:00 -0500

    First commit

--- main.py
+++ main.py
@@ -4,70 +4,28 @@ import torch.nn as nn
 import torch.optim as optim
 from model import model
 from loss import BatchHardTripLoss
+from Evaluation import evaluation
 from torch.autograd import Variable
 from torchvision import transforms
 from sklearn.datasets import fetch_lfw_people
-from utils import SuperList, GenIdx, prepare_set, IdentitySampler, \
-    prepare_dataset, prepare_data_ids,LFW_training_Data, AverageMeter
+from utils import GenIdx,IdentitySampler, prepare_dataset, prepare_set, \
+                    prepare_data_ids,LFW_training_Data, AverageMeter
 import numpy as np
-import random
 
-# Work on GPU if available
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+#### Fonction utiles ####
 
-# Import the lfw dataset - With 15 img min per person
-print("===> Loading Fetch Dataset \n")
-lfw_people = fetch_lfw_people(min_faces_per_person=15, resize=0.4)
-nb_img_per_id_to_keep = 15
-X, y, n_classes = prepare_dataset(lfw_people, nb_img_per_id_to_keep)
+# Fonction d'entraînement
+def train(epoch, criterion_id, criterion_tri, optimizer, trainloader, device, net):
 
-print("Dataset Loaded")
-
-# Get train / val folds ids and get test ids
-train_ids_lists, val_ids_lists, test_ids = prepare_data_ids(n_classes)
-
-print("")
-# Prepare training data
-num_img_of_same_id_in_batch = 4
-num_different_identities_in_batch = 8
-batch_size = num_img_of_same_id_in_batch * num_different_identities_in_batch
-
-global_img_pos = GenIdx(y)  # Get the images positions in list for each specific identity
-
-folds = 5
-
-# epochs = "nb_epoch_to_define"
-epochs = 10
-
-# img_h, img_w = 224, 224
-
-transform_train = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.Pad(10),
-    # transforms.RandomCrop((img_h, img_w)),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-])
-
-# Criterion / loss :
-criterion_id = nn.CrossEntropyLoss().to(device)
-criterion_tri = BatchHardTripLoss(batch_size=batch_size, margin= 0.3).to(device)
-
-
-
-def train(epoch):
-    # extract features
-    # Compute loss
-    # Update weights
     train_loss = AverageMeter()
     id_loss = AverageMeter()
     tri_loss = AverageMeter()
-    Channel_exchange_loss = AverageMeter()
     data_time = AverageMeter()
     batch_time = AverageMeter()
     correct = 0
     total = 0
     end = time.time()
+    # Pour chaque batch
     for batch_idx, (inputs, labels) in enumerate(trainloader):
         # Labels 1 and 2 are the same because the two inputs correspond to the same identity
 
@@ -80,8 +38,8 @@ def train(epoch):
 
         feat, out0 = net(inputs)
 
-        loss_ce = criterion_id(out0, labels)
-        loss_tri, batch_acc = criterion_tri(feat, labels)
+        loss_ce = criterion_id("A remplir")
+        loss_tri, batch_acc = criterion_tri("A remplir")
         _, predicted = out0.max(1)
 
 
@@ -95,55 +53,151 @@ def train(epoch):
 
         total += labels.size(0)
 
+        # Update loss for display
+        train_loss.update(loss.item(), inputs.size(0))
+        id_loss.update(loss_ce.item(), inputs.size(0))
+        tri_loss.update(loss_tri.item(), inputs.size(0))
+
         # measure elapsed time
         batch_time.update(time.time() - end)
         end = time.time()
-        if batch_idx % 50 == 0:
+        if batch_idx % 10 == 0:
+            print(inputs.size(0))
             print(f'Epoch: [{epoch}][{batch_idx}/{len(trainloader)}] '
                   f'Time: {batch_time.val:.3f} ({batch_time.avg:.3f}) '
-                  # f'lr:{current_lr:.3f} '
                   f'Loss: {train_loss.val:.4f} ({train_loss.avg:.4f}) '
                   f'iLoss: {id_loss.val:.4f} ({id_loss.avg:.4f}) '
                   f'TLoss: {tri_loss.val:.4f} ({tri_loss.avg:.4f}) '
-                  # f'Channel Exchange Loss : ({Channel_exchange_loss.val:.4f}) ({Channel_exchange_loss.avg:.4f})'
                   f'Accu: {100. * correct / total:.2f}')
 
-    pass
+# Fonction permettant la validation
 def valid(epoch):
     # Prepare query gallery sets
-    # Extract features
-    # Compare features (Similarity measure)
-    # Compute metrics (Rank-1, Rank-5, mAP)
+
     # Eventually save the model if this one is the best overall
-    pass
 
-for fold in range(folds):
-    net = model(n_classes).to(device)
+    # Get the timer
+    end = time.time()
 
-    # Optimizer :
-    lr = 0.1
-    optimizer = optim.SGD([
-        {'params': net.parameters(), 'lr': lr}],
-        weight_decay=5e-4, momentum=0.9, nesterov=True)
+    # Extract features from query and gallery
+    query_gall_feat_pool, query_gall_feat= extract_query_gall_feat(query_gall_loader, n_query_gall, net=net)
 
-    for epoch in range(epochs):
-        trainset = LFW_training_Data(X, global_img_pos, train_ids_lists[fold], nb_img_per_id_to_keep,\
-                                     transform=transform_train)
+    print(f"Feature extraction time : {time.time() - end}")
+    start = time.time()
 
-        img_pos = GenIdx(trainset.train_labels)  # Get the images positions in list for each specific identity
+    # Compute the similarity (cosine)
+    distmat_pool = np.matmul(query_gall_feat_pool, np.transpose(query_gall_feat_pool))
+    distmat_fc = np.matmul(query_gall_feat, np.transpose(query_gall_feat))
 
-        sampler = IdentitySampler(trainset.train_labels, img_pos, num_img_of_same_id_in_batch,
-                                  num_different_identities_in_batch)
-        trainset.cIndex = sampler.index
+    # Evaluation - Compute metrics (Rank-1, Rank-5, mAP)
 
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,\
-                                                  sampler=sampler, drop_last=True)
+    cmc_att, mAP_att, mINP_att  = evaluation(-distmat_pool, query_gallery_labels) # On avg_pool features
+    cmc, mAP, mINP = evaluation(-distmat_fc, query_gallery_labels)                # On batch normed features
+
+    print('Evaluation Time:\t {:.3f}'.format(time.time() - start))
+
+    return cmc, mAP, mINP, cmc_att, mAP_att, mINP_att
 
 
-        train(epoch)
+if __name__ == "__main__":
 
-        if epoch != 0 and epoch % 2 == 0:
-            valid(epoch)
-        break
+    # Work on GPU if available
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# Eventually here test all models
+    # Import the lfw dataset - With 15 img min per person
+    print("===> Loading Fetch Dataset \n")
+    lfw_people = fetch_lfw_people(min_faces_per_person=15, resize=0.4)
+    nb_img_per_id_to_keep = 15
+
+    # Renvoie la base de données avec 15 images par personnes maximum
+    # Vous devrez coder la fonction dans le fichier utils
+    X, y, n_classes = prepare_dataset(lfw_people, nb_img_per_id_to_keep)
+
+
+    # Produire les listes d'identitées qui correspondront aux folds d'entraînement / validation et
+    # aux identitées de test.
+    # train_ids_lists et val_ids_lists seront des listes de 5 listes d'identitées (Une liste pour chaque fold)
+    # test_ids sera une liste de 21 identitées
+    train_ids_lists, val_ids_lists, test_ids = prepare_data_ids(n_classes)
+
+    # Prepare var - Batch size
+    num_img_of_same_id_in_batch = 4
+    num_different_identities_in_batch = 8
+    batch_size = num_img_of_same_id_in_batch * num_different_identities_in_batch
+    global_img_pos = GenIdx(y)  # Get the images positions in list for each specific identity
+
+    folds = "A remplir"
+    epochs = "A remplir"
+
+    # img_h, img_w = 224, 224
+
+    transform_train = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Pad(10),
+        # transforms.RandomCrop((img_h, img_w)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+    ])
+
+    # Définir vos fonctions de pertes (Cross-entropy + Fonction de pertes pour réseaux siamois)
+    # Vous pouvez définir votre fonction de pertes pour réseaux siamois dans le fichier loss.py
+    criterion_id = "A remplir"
+    criterion_tri = "A remplir"
+
+    for fold in range(folds):
+        net = model(n_classes).to(device)
+
+        # Définir votre optimizer :
+        opt = "A remplir"
+
+        # Preparez votre query / gallery set
+        gallset, n_query_gall = prepare_set(val_ids_lists[fold], nb_img_per_id_to_keep, transform=transform_test,
+                                            img_size=(224, 224))
+
+        query_gall_loader = torch.utils.data.DataLoader(gallset, batch_size=32, shuffle=False)
+
+        for epoch in range(epochs):
+
+            trainset = LFW_training_Data(X, global_img_pos, train_ids_lists[fold], nb_img_per_id_to_keep, \
+                                         transform=transform_train)
+
+            img_pos = GenIdx(trainset.train_labels)  # Get the images positions in list for each specific identity
+
+            sampler = IdentitySampler(trainset.train_labels, img_pos, num_img_of_same_id_in_batch,
+                                      num_different_identities_in_batch)
+
+            trainset.cIndex = sampler.index
+
+            trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, \
+                                                      sampler=sampler, drop_last=True)
+
+            train(epoch, criterion_id, criterion_tri, opt, trainloader, device, net)
+
+            # Call the validation every two epochs
+            if epoch != 0 and epoch % 2 == 0:
+                cmc, mAP, mINP, cmc_att, mAP_att, mINP_att = valid(epoch)
+
+                # Save model based on validation mAP or mINP ?
+                if mAP > best_map:  # Usual saving
+
+                    # best_acc = cmc_att[0]
+                    best_map = mAP
+                    best_minp = mINP
+                    best_epoch = epoch
+                    state = {
+                        'net': net.state_dict(),
+                        'cmc': cmc,
+                        'mAP': mAP,
+                        'mINP': mINP,
+                        'epoch': epoch,
+                    }
+                    torch.save(state, f'model_fold({fold})_best.t')
+
+                print('fc : Rank-1: {:.2%} | Rank-5: {:.2%} | mAP: {:.2%}| mINP: {:.2%}'.format(cmc[0], cmc[4], mAP,
+                                                                                                mINP))
+                print('att : Rank-1: {:.2%} | Rank-5: {:.2%} | mAP: {:.2%}| mINP: {:.2%}'.format(cmc_att[0], cmc_att[4],
+                                                                                                 mAP_att, mINP_att))
+
+    # Tester ici ou dans un fichier à part votre modèle sur les données de test.
+
+
--- utils.py
+++ utils.py
@@ -4,72 +4,39 @@ from torch.utils.data.sampler import Sampler
 import torch.utils.data as data
 import random
 
+# Renvoie la base de données d'images sous X et les labels sous y
+# Conserve uniquement 15 images par personnes maximum
 def prepare_dataset(lfw_people, nb_img_per_id_to_keep):
     n_samples, h, w = lfw_people.images.shape
 
     X_init = lfw_people.images
     y_init = lfw_people.target
 
-    n_features = X_init.shape[1]
+    """
+    
+    Code attendu :
+    Votre base X, y à définir. 
+    
+    """
 
-    target_names = lfw_people.target_names
-    n_classes = target_names.shape[0]
+    X, y, n_classes = None, None
 
-    print("Total dataset size:")
-    print("n_samples: %d" % n_samples)
-    print("n_features: %d" % n_features)
-    print("n_classes: %d" % n_classes)
-
-    X = np.zeros((n_classes * nb_img_per_id_to_keep, 50, 37))
-    y = [0 for _ in range(nb_img_per_id_to_keep)]
-    for idx, label in enumerate(np.unique(y_init)):  # Pour chaque personne
-        person_idx = label == y_init
-        X_label = X_init[person_idx, :]
-        random_selection = np.random.randint(X_label.shape[0], size=nb_img_per_id_to_keep) # Get 15 ids from the available ones
-
-        if idx == 0:
-            X = X_label[random_selection, :]
-        else:
-            X = np.concatenate((X, X_label[random_selection, :]), axis=0)
-            y.extend([label for _ in range(nb_img_per_id_to_keep)])
-
-    return (X, y, n_classes)
+    return X, y, n_classes
 
 
 def prepare_data_ids(n_classes):
-    n_classes_trainval = int(0.8 * n_classes) - 1  # We suppress one id so that it n_classes_trainval%5=0
-    print(n_classes_trainval)
-    # Get the number of class needed in each set
-    n_classes_test = n_classes - n_classes_trainval
-    n_classes_train = int(0.8 * n_classes_trainval)
-    n_classes_val = n_classes_trainval - n_classes_train
 
-    print(f"n_classes_train : {n_classes_train}")
-    print(f"n_classes_val : {n_classes_val}")
-    print(f"n_classes_test : {n_classes_test}")
+    """
 
-    # Random class selection generation
-    shuffled_classes = random.sample([i for i in range(n_classes)], n_classes)
+    Code attendu :
+    Définissez train_ids_lists, val_ids_lists, test_ids_list
 
-    test_ids_list = shuffled_classes[:n_classes_test]
-    trainval_classes = shuffled_classes[n_classes_test:]
+    """
 
-    # Prepare trainval ids for 5 folds
-    trainval_classes_super = SuperList(trainval_classes)
-    train_ids_lists, val_ids_lists = [[] for _ in range(5)], [[] for _ in range(5)]
-    for i in range(5):
-        val_ids_lists[i] = trainval_classes_super[15 * i:15 * (i + 1)]
-        train_ids_lists[i] = trainval_classes_super[15 * i:15 * (i + 1):'c']
+    train_ids_lists, val_ids_lists, test_ids_list = None, None, None
+
     return train_ids_lists, val_ids_lists, test_ids_list
 
-class SuperList(list):
-    def __getitem__(self, val):
-        if type(val) is slice and val.step == 'c':
-            copy = self[:]
-            copy[val.start:val.stop] = []
-            return copy
-
-        return super(SuperList, self).__getitem__(val)
 
 def GenIdx(train_label):
     color_pos = []
@@ -112,7 +79,7 @@ class IdentitySampler(Sampler):
     def __len__(self):
         return self.N
 
-def prepare_set(ids, nb_images_per_id, images_X, labels_y):
+def prepare_set(ids, nb_images_per_id, images_X):
     # Query and gallery are the same since we want to compare query to all gallery image
     query_gallery_img = np.zeros((len(ids) * nb_images_per_id, 50, 37))
 
@@ -126,35 +93,35 @@ def prepare_set(ids, nb_images_per_id, images_X, labels_y):
             query_gallery_img = np.concatenate((query_gallery_img, sublist), axis=0)
             label_query_gallery.extend([id for _ in range(nb_images_per_id)])
 
-    return query_gallery_img, np.array(label_query_gallery)
+    return query_gallery_img, np.array(label_query_gallery), len(label_query_gallery)
 
 
 
 class LFW_training_Data(data.Dataset):
     def __init__(self, X, img_pos, img_ids_to_extract, nb_img_per_id, transform=None, colorIndex=None):
 
-        nb_imgs_to_extract = len(img_ids_to_extract)
-        relabel = []
-        X_to_keep = None
-        for idx, img_id_to_extract in enumerate(img_ids_to_extract):
-            first_loc = img_pos[img_id_to_extract][0]
-            last_loc = img_pos[img_id_to_extract][-1]
-            if idx == 0:
-                X_to_keep = X[first_loc:last_loc + 1]
-                relabel = [idx for _ in range(nb_img_per_id)]
-            else:
-                X_to_keep = np.concatenate((X_to_keep, X[first_loc:last_loc+1]), axis=0)
-                relabel.extend([idx for _ in range(nb_img_per_id)])
+        """
 
-        self.train_images = X_to_keep
-        self.train_labels = relabel
+        Code attendu :
+
+        A partir des identitées d'entraînement pour un fold donné (img_ids_to_extract):
+        self.train_images devra être la sous base de X contenant uniquement les ids de img_ids_to_extract
+        self.train_labels devra être la sous base de labels. Il est conseillé de re-labeliser les images, en
+        partant de 0. C'est à dire que les 15 premières images de la sous bases seront associées au nouveau label 0,
+        les 15 suivantes au nouveau label 1 etc...
+
+        """
+
+
+        self.train_images = "X_to_keep"
+        self.train_labels = "relabel"
 
         self.transform = transform
         # Prepare index
         self.cIndex = colorIndex
 
     def __getitem__(self, index):
-        #Dataset[i] return images from both modal and the corresponding label
+        #Dataset[i] return image with its corresponding label
         img, target = self.train_images[self.cIndex[index]], self.train_labels[self.cIndex[index]]
         img = self.transform(img)
         return img, target
